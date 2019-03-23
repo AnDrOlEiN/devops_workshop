799d78e83e1d41abad95c993dceb709551e3601f
AnDrOlEiN <syrogk@gmail.com>
2019-03-23 10:50:09 +0300

    first checkpoint reached

--- make-app.mk
+++ make-app.mk
@@ -1,11 +1,14 @@
 # using root to have access to db folder during deps installing
 USER = nobody
 
+app:
+	docker-compose up
+
 app-setup: app-build
 	docker-compose run --user=$(USER) app make install
 
 app-build:
 	docker-compose build
 
-app:
-	docker-compose up
+clean:
+	docker-compose down
