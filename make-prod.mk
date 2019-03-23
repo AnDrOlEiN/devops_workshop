cc83806d96dc49e64c559b84cc0e2bcba937196b
AnDrOlEiN <syrogk@gmail.com>
2019-03-23 22:27:07 +0300

    ready to deploy version

---
+++ make-prod.mk
@@ -0,0 +1,13 @@
+U := me
+
+production-setup:
+	ansible-playbook ansible/site.yml -i ansible/production --ask-become-pass
+
+production-env-update:
+	ansible-playbook ansible/site.yml -i ansible/production -u $U --tag env --ask-become-pass
+
+production-deploy:
+	ansible-playbook ansible/deploy.yml -i ansible/production -u $U --ask-vault-pass
+
+production-deploy-app:
+	ansible-playbook ansible/deploy.yml -i ansible/production -u $U --tag app --ask-vault-pass
