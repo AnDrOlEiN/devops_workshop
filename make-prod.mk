3f3bad2d6472f9d8e8a59d58f274451324a644a2
AnDrOlEiN <syrogk@gmail.com>
2019-03-24 11:38:25 +0300

    added second droplet

--- make-prod.mk
+++ make-prod.mk
@@ -3,11 +3,17 @@ U := me
 production-setup:
 	ansible-playbook ansible/site.yml -i ansible/production --ask-become-pass --ask-vault-pass
 
+production-setup-second:
+	ansible-playbook ansible/site.yml -i ansible/production --ask-become-pass --ask-vault-pass --limit "production_second"
+
 production-env-update:
 	ansible-playbook ansible/site.yml -i ansible/production -u $U --tag env --ask-become-pass --ask-vault-pass
 
 production-deploy:
 	ansible-playbook ansible/deploy.yml -i ansible/production -u $U --ask-vault-pass
+
+production-deploy-second:
+	ansible-playbook ansible/deploy.yml -i ansible/production -u $U --ask-vault-pass --limit "production_second"
 
 production-deploy-app:
 	ansible-playbook ansible/deploy.yml -i ansible/production -u $U --tag app --ask-vault-pass
